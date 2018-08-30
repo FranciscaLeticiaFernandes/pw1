206a7500fa56b9d1c617895da0338755f9cfc4a6
FranciscaLeticiafernandes <lele.lf50@gmail.com>
2018-08-30 20:43:35 -0300

    aula de funçao

---
+++ javascript/aula2/new2.js
@@ -0,0 +1,8 @@
+function multiplica(a,b){
+console.log("function multiplica(a,b){...");
+var resultado = a*b;
+console.log("imprimindo resultado")
+return a*b;
+}
+var resultadodafuncao = multiplica(5,2);
+alert(resultadodafuncao);
