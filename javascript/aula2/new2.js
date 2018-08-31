42e7aba10a99ecf52241b68cf371a43aed7f2b78
FranciscaLeticiafernandes <lele.lf50@gmail.com>
2018-08-30 22:37:14 -0300

    funçao e for

--- javascript/aula2/new2.js
+++ javascript/aula2/new2.js
@@ -5,4 +5,14 @@ console.log("imprimindo resultado")
 return a*b;
 }
 var resultadodafuncao = multiplica(5,2);
-alert(resultadodafuncao);
+//alert(resultadodafuncao);
+
+function paiscomp (paises){
+	var i;
+	for(i = 0; i < paises.length;i++){
+		console.log(paises[i]);
+	}
+}
+var paises = ["prussia","patagonia","peru","polonia","paquistao","portugal"];
+paiscomp (paises);
+console.log("quantidade de paises com p:"+paises.length);
